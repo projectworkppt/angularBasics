ca9756fb3c411b2bc90c38b390e7e4b797bf4e8a
Project Work PPT <70477983+projectworkppt@users.noreply.github.com>
2020-09-01 12:16:18 +0530

    Add files via upload

---
+++ src/app/components/uppercase-converter/uppercase-converter.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {UppercaseConverterService} from '../../services/uppercase-converter.service';
+import {HttpErrorResponse} from '@angular/common/http';
+
+@Component({
+  selector: 'app-uppercase-converter',
+  templateUrl: './uppercase-converter.component.html',
+  styleUrls: ['./uppercase-converter.component.scss']
+})
+export class UppercaseConverterComponent implements OnInit {
+  private result:any;
+
+  constructor(private _service:UppercaseConverterService) { }
+
+  ngOnInit(): void {
+  }
+  public convert(obj):any{
+    this._service.convertToUppercase(obj).subscribe(
+      res=>this.result=res,
+      (err:HttpErrorResponse)=>{
+        console.error(err);
+      }
+    )
+  }
+
+}
